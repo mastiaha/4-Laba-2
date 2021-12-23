149f839016aebdb5eb68a367c99b5dc73e5210a6
mastiaha <mastiaha@mail.ru>
2021-12-23 20:00:28 +0300

    Лекция 4

--- 4 Laba/Program.cs
+++ 4 Laba/Program.cs
@@ -14,7 +14,11 @@ namespace _4_Laba
             for (int i = 0; i < 10; ++i)
 
             {
-                              
+                if (i==5)
+                
+                    continue;
+
+                
                 Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
             }
             Console.ReadKey();
---
+++ Задача 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1, sqr = 0; i <=n; i++)
+
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, sqr += 2 * i - 1);
+               
+            }
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+        
+    }
+}
